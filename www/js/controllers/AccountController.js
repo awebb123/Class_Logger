bc1e6e5e92d633118c3aece409cea52ddde599d8
Alex Webb <alex@positivelogic.biz>
2016-09-03 14:44:39 -0400

    Add files via upload

--- www/js/controllers/AccountController.js
+++ www/js/controllers/AccountController.js
@@ -2,10 +2,6 @@ angular.module('cp')
 
     .controller('AccountCtrl', ['$scope', function ($scope) {
         console.log('AccountCtrl here');
-        $scope.openFab = function () {
-            console.log('firing');
-            $('.fixed-action-btn').openFAB();
-        };
 
         $scope.sideNav = function () {
             console.log('SideNav');
