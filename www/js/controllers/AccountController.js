0402c6c448ea122f2a3352ddf99b2afa3692d796
Alex Webb <alex@positivelogic.biz>
2016-09-30 11:03:29 -0400

    Added an image path for the cards on the about page

--- www/js/controllers/AccountController.js
+++ www/js/controllers/AccountController.js
@@ -3,9 +3,10 @@ angular.module('cp')
     .controller('AccountCtrl', ['$scope', function ($scope) {
         console.log('AccountCtrl here');
 
+        $scope.imagePath = '../../imgs/icons/account_icon.svg';
+
         $scope.sideNav = function () {
             console.log('SideNav');
             $(".button-collapse").sideNav();
         }
-
     }]);
