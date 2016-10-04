80bba51714870bec5146c2c56eb83b9af97f8577
Alex Webb <alex@positivelogic.biz>
2016-10-04 11:47:58 -0400

    Updated the AccountController.js file to include a path for an image

--- www/js/controllers/AccountController.js
+++ www/js/controllers/AccountController.js
@@ -3,7 +3,7 @@ angular.module('cp')
     .controller('AccountCtrl', ['$scope', function ($scope) {
         console.log('AccountCtrl here');
 
-        $scope.imagePath = '../../imgs/icons/account_icon.svg';
+        $scope.imagePath = '../../imgs/icons/dev.jpg';
 
         $scope.sideNav = function () {
             console.log('SideNav');
